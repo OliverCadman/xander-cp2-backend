ecbf1de0f0d042cd7cc2467380570a5f663ae426
Oliver Cadman <o.cadman@live.co.uk>
2023-02-23 10:02:03 +0000

    Added inlines to models in admin interface

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,11 +5,34 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
+class LessonInline(admin.TabularInline):
+    """View lessons for a given module."""
+
+    model = models.Lesson
+    classes = ['collapse']
+
+
 class TextBlockInline(admin.TabularInline):
     """View TextBlocks in Lesson/Exercise Admin Pages"""
 
     model = models.TextBlock
     classes = ['collapse']
+
+
+class TopicInline(admin.TabularInline):
+    """View Topics for a given Module"""
+
+    model = models.Topic
+    classes = ['collapse']
+
+
+class ModuleInline(admin.TabularInline):
+    """View Modules for a given Language."""
+
+    model = models.Module
+    classes = ['collapse']
+
 
 class LessonAdmin(admin.ModelAdmin):
 
@@ -19,6 +42,24 @@ class LessonAdmin(admin.ModelAdmin):
 class ExerciseAdmin(admin.ModelAdmin):
 
     inlines = [TextBlockInline]
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    """Custom admin interface for modules"""
+    
+    inlines = [TopicInline]
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    """Custom admin interface for languages.""" 
+
+    inlines = [ModuleInline]
+
+
+class TopicAdmin(admin.ModelAdmin):
+    """Custom admin interface for topics."""
+
+    inlines = [LessonInline]
 
 
 class UserAdmin(BaseUserAdmin):
@@ -61,9 +102,9 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.UserProfile)
-admin.site.register(models.Language)
-admin.site.register(models.Module)
-admin.site.register(models.Topic)
+admin.site.register(models.Language, LanguageAdmin)
+admin.site.register(models.Module, ModuleAdmin)
+admin.site.register(models.Topic, TopicAdmin)
 admin.site.register(models.TextBlock)
 admin.site.register(models.Exercise, ExerciseAdmin)
 admin.site.register(models.Lesson, LessonAdmin)
